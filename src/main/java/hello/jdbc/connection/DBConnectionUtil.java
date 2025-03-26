655ea936e26af9fc92df890e9d785cf037fd3fa0
omea92 <thdjun92@naver.com>
2025-03-27 00:15:21 +0900

    Connection, HikariCP 정리 주석반여

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -10,6 +10,8 @@ public class DBConnectionUtil {
     // DB 연동 객체
     public static Connection getConnection() {
         try {
+            // 기존 DataSource 는 호출마다 연동작업이 수행되어 메모리 낭비 심함
+            // 보완으로 HikariCP 사용 : 스프링부트는 내장되어 자동지원, 호출때 한번만 수행, 설정과 사용을 분리
             Connection connection = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
             log.info("get connection={}, class={}", connection, connection.getClass());
             return connection;
