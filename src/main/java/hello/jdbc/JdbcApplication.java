b662955c4392bb37ab97de23a8503e9e0da151d8
himchan92 <omea92@naver.com>
2025-03-27 13:00:37 +0900

    자바 예외처리 개념 정리 주석

--- src/main/java/hello/jdbc/JdbcApplication.java
+++ src/main/java/hello/jdbc/JdbcApplication.java
@@ -13,6 +13,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
     스프링 트랜젝션
     - @Transactionoal : 시작과 끝을 유지하려면 같은 DB 커넥션 유지를 위해 동기화 지원
+
+    기존 자바 예외처리
+    - 체크예외 : 예외를 던지는 throws 처리 필수이며 안하면 오류
+    - 언체크예외 : throws 없이도 예외처리(실무에서 자주사용)
+    - Exception : 최상위 예외로 모든 예외 컴파일러가 체크
+    - throws Exception : 하위 예외모두 던지기 가능
+    - 예외처리 안할 시 시스템에서 오류를 안잡고 바로 종료 시켜버림
+    - 메소드 throws : 메소드밖으로 예외처리를 던짐
+    - 장점 : 개발자의 실수로 누락된 예외처리를 시스템이 잡아서 예외체크
+    - 단점 : 개발자가 예외를 잡거나 던지도록 로직 처리 필요
+    - 실무에서 예외 메세지는 로그를 주로 사용하여 권장
+       - e.printStackTrace() : 비추천
+         log("e: " + e);     : 추천
  */
 @SpringBootApplication
 public class JdbcApplication {
